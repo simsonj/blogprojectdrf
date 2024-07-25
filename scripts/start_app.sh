8880d67657d25dbdf28c905fab2286c602372887
Simson J <simsonja2@gmail.com>
2024-07-25 11:40:54 +0530

    update

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["Your_Instance_Public_IP"]/' /home/ubuntu/Your_project_folder_name/mysite/settings.py
+sed -i 's/\[]/\["35.76.113.20"]/' /home/ubuntu/blogprojectdrf/mysite/settings.py
 
 python manage.py migrate 
 # python manage.py makemigrations     
